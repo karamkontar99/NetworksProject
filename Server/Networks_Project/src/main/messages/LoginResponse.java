6a85d31477b55cdbb40adbe6708d821a1731a0e8
AliAyash <alihayyash@gmail.com>
2019-12-11 20:45:08 +0200

    added login, registration, fileupload response

--- Server/Networks_Project/src/main/messages/LoginResponse.java
+++ Server/Networks_Project/src/main/messages/LoginResponse.java
@@ -6,29 +6,15 @@ public class LoginResponse {
 	public int status;
 
 	public void parseFromByteArray(byte[] bytes) throws Exception {
-		int index = 0;
-
-		byte[] statusLengthB = Arrays.copyOfRange(bytes, index, index + 4);
-		index += 4;
-		int statusLength = ByteBuffer.wrap(statusLengthB).getInt();
-
-		byte[] statusB = Arrays.copyOfRange(bytes, index, index + statusLength);
-		index += statusLength;
-		String status = new String(statusB, StandardCharsets.US_ASCII);
-
+		byte[] statusB = Arrays.copyOfRange(bytes, 0, 4);
+		int status = ByteBuffer.wrap(statusB).getInt();
 		this.status = status;
 	}
 
 	public byte[] serializeToByteArray() {
-		int index = 0;
-		byte[] bytes = new byte[4 + status.length()];
-
-		ByteBuffer.wrap(bytes, 0, 4).putInt(status.length());
-		index += 4;
-		byte[] statusBytes = this.status.getBytes(StandardCharsets.US_ASCII);
-		System.arraycopy(statusBytes, 0, bytes, index, status.length());
-		index += status.length();
-
+		byte[] bytes = new byte[4];
+		ByteBuffer.wrap(bytes, 0, 4).putInt(status);
 		return bytes;
 	}
+
 }
