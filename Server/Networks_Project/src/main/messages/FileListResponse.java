9d135c2266db949f6d9bb25dbc610bfd412e6b60
a1darm <ali.mrad03@hotmail.com>
2019-12-12 10:31:10 +0200

    updated ENUM for fileList upload and download

--- Server/Networks_Project/src/main/messages/FileListResponse.java
+++ Server/Networks_Project/src/main/messages/FileListResponse.java
@@ -1,26 +1,68 @@
 package main.messages;
 
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
-public class FileListResponse implements MessageInterface {
+public class FileListResponse implements  MessageInterface{
+    public int listsize;
+    public int ItemLenght;
+    public String ItemName;
+    public int NextItemSize;
 
-    public int status;
+    public void parseFromByteArray(byte[] bytes) throws Exception {
+        int index = 0;
+
+        byte[] listsizeB = Arrays.copyOfRange(bytes, index, index + 4);
+        int listsize = ByteBuffer.wrap(bytes, index, index + 4).getInt();
+        index += 4;
+
+        byte[] ItemLengthB = Arrays.copyOfRange(bytes, index, index + 4);
+        int ItemLength = ByteBuffer.wrap(bytes, index, index + 4).getInt();
+        index += 4;
+
+        byte[] ItemNamelengthB = Arrays.copyOfRange(bytes, index, index + 4);
+        int ItemNamelength = ByteBuffer.wrap(ItemNamelengthB).getInt();
+        index += 4;
+
+        byte[] ItemNameB = Arrays.copyOfRange(bytes, index, index + ItemNamelength);
+        String fileName = new String(ItemNameB, StandardCharsets.US_ASCII);
+        index += ItemNamelength;
+
+
+        byte[] NextItemSizeB = Arrays.copyOfRange(bytes, index, index + 4);
+        int NextItemSize = ByteBuffer.wrap(bytes, index, index + 4).getInt();
+        index += 4;
+
+        this.listsize = listsize;
+        this.ItemLenght = ItemLength;
+        this.ItemName = fileName;
+        this.NextItemSize = NextItemSize;
+
+    }
 
     @Override
     public EMsg getEMsg() {
-        return EMsg.EFileUploadResponse;
-    }
-
-    public void parseFromByteArray(byte[] bytes) throws Exception {
-        byte[] statusB = Arrays.copyOfRange(bytes, 0, 4);
-        int status = ByteBuffer.wrap(statusB).getInt();
-        this.status = status;
+        return  EMsg.EFileListResponse;
     }
 
     public byte[] serializeToByteArray() {
-        byte[] bytes = new byte[4];
-        ByteBuffer.wrap(bytes, 0, 4).putInt(status);
+        int index = 0;
+        byte[] bytes = new byte[4 + 4 + 4 + 4 + listsize + ItemLenght + ItemName.length() + NextItemSize];
+
+        ByteBuffer.wrap(bytes, 0, 4).putInt(listsize);
+        index += 4;
+
+        ByteBuffer.wrap(bytes, index, 4).putInt(ItemLenght);
+        index += 4;
+
+        byte[] fileNameBytes = this.ItemName.getBytes(StandardCharsets.US_ASCII);
+        System.arraycopy(fileNameBytes, 0, bytes, index, ItemName.length());
+        index += ItemName.length();
+
+        ByteBuffer.wrap(bytes, index, 4).putInt(NextItemSize);
+        index += 4;
+
         return bytes;
     }
 }
